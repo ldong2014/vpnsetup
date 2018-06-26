a6bc28b60d5204ee3dea988e51ecb38ef87176e2
Zhang Huanjie <james@ustc.edu.cn>
2018-06-26 19:35:33 +0800

    remote slave use the same port

--- vpnrestart.sh
+++ vpnrestart.sh
@@ -67,11 +67,11 @@ else
 	REMOTE2=$(cat /etc/ethudp/SITE/$SLAVE)
 	PORT2=`expr $PORT + 1000`
 	MPORT2=`expr $PORT2 + 100`
-	/usr/src/ethudp/EthUDP -e $OPT $IP $PORT $REMOTE $PORT eth1 $IP $PORT2 $REMOTE2 $PORT2
-	/usr/src/ethudp/EthUDP -i $OPT $IP $MPORT $REMOTE $MPORT $PREFIX$INDEX.2 24 $IP $MPORT2 $REMOTE2 $MPORT2
+	/usr/src/ethudp/EthUDP -e $OPT $IP $PORT $REMOTE $PORT eth1 $IP $PORT2 $REMOTE2 $PORT
+	/usr/src/ethudp/EthUDP -i $OPT $IP $MPORT $REMOTE $MPORT $PREFIX$INDEX.2 24 $IP $MPORT2 $REMOTE2 $MPORT
 	if [ $IPV6 = "YES" ]; then
 		PORT=`expr $PORT + 500`
 		PORT2=`expr $PORT + 1000`
-		/usr/src/ethudp/EthUDP -e $OPT $IP $PORT $REMOTE $PORT eth2 $IP $PORT2 $REMOTE2 $PORT2
+		/usr/src/ethudp/EthUDP -e $OPT $IP $PORT $REMOTE $PORT eth2 $IP $PORT2 $REMOTE2 $PORT
 	fi
 fi
